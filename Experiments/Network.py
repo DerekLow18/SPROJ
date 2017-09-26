0b3c2d5a21d688bbc464cf43f210aac7b76e0c7a
Derek <dlow148@gmail.com>
2017-09-25 23:21:32 -0400

    170925

--- Experiments/Network.py
+++ Experiments/Network.py
@@ -6,13 +6,13 @@ import matplotlib.pyplot as plt
 
 #generate all nodes
 #neurons
-pop1 = nest.Create("iaf_psc_alpha", 50)
+pop1 = nest.Create("iaf_psc_alpha", 10)
 nest.SetStatus(pop1, {"I_e": 376.0})
 #multimeter to detect membrance potential
 multimeter = nest.Create("multimeter")
 nest.SetStatus(multimeter, {"withtime":True, "record_from":["V_m"]})
 
-Ex = 20
+Ex = 2
 d = 1.0
 
 conn_dict = {"rule": "fixed_indegree", "indegree": Ex} #connection dictionary
@@ -26,7 +26,8 @@ nest.Connect(multimeter, [1])
 #pop1_layer = topp.CreateLayer(pop1)
 #nest.PrintNetwork()s
 
-#print(nest.GetConnections())
+pop1_connect_dict = nest.GetConnections(pop1)
+#print(pop1_connect_dict[0])
 
 nest.Simulate(1000.0)
 mmStatus = nest.GetStatus(multimeter)[0]
@@ -37,8 +38,14 @@ pylab.figure(1)
 pylab.plot(ts, Vms)
 
 pylab.figure(2)
-G = nx.Graph()
+G = nx.DiGraph()
 for i in pop1:
 	G.add_node(i)
+netXEdges = []
+for j in pop1_connect_dict:
+	x = j[0]
+	y = j[1]
+	netXEdges.append((x,y))
+G.add_edges_from(netXEdges)
 nx.draw(G, with_labels=True)
 plt.show()

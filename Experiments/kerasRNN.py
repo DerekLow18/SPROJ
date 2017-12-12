9ddbbb3af276fcd7a763a4112ae93fa9b51fccf5
Derek <dlow148@gmail.com>
2017-12-12 11:14:59 -0500

    Keras is working i think
    Former-commit-id: b35f49d9f4c040699967d607f4807b507a799e0a

--- Experiments/kerasRNN.py
+++ Experiments/kerasRNN.py
@@ -10,6 +10,7 @@ import pandas
 import math
 import keras
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
 
 # convert an array of values into a dataset matrix
 def create_dataset(dataset, look_back=1):
@@ -23,52 +24,56 @@ def create_dataset(dataset, look_back=1):
 dataframe = pandas.read_csv('./Spike Results/1idTimes.csv')
 dataset = dataframe.values
 dataset = dataset.astype('float32')
-scaler = MinMaxScaler(feature_range = (0,1))
-dataset = scaler.fit_transform(dataset)
+#scaler = MinMaxScaler(feature_range = (0,1))
+#dataset = scaler.fit_transform(dataset)
+print(dataset)
 dataset = np.transpose(dataset)
+print(len(dataset))
 
 train_size = int(len(dataset) * 0.67)
 test_size = len(dataset) - train_size
 train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
 
-batchSize = 1
-trainX, trainY = create_dataset(train,batchSize)
-testX, testY = create_dataset(test,batchSize)
+batchSize = 9
+trainX, trainY = train[0:len(train)-1], train[1:len(train)]
+print(trainX[58])
+print(trainY[57])
+testX, testY = test[0:len(test)-1], test[1:len(test)]
+#print(trainX[0])
+print(trainX.shape)
+print(testX.shape)
 trainX = np.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
 testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+print(trainX.shape)
+print(trainY.shape)
 
-#Create the network here. Each hidden layer has 4 LSTM blocks, with 1 input, and a single output layer
+#Create the network here. Each hidden layer has 10 LSTM blocks, with 1 input, and a single output layer
 model = keras.models.Sequential()
-model.add(keras.layers.LSTM(4,input_shape=(1, batchSize)))
-model.add(keras.layers.Dense(1))
+model.add(keras.layers.LSTM(10,input_shape=(1, batchSize),return_sequences=True))
+model.add(keras.layers.Flatten())
+model.add(keras.layers.Dense(9))
+model.add(keras.layers.ThresholdedReLU(theta = 0.5))
 model.compile(loss='mean_squared_error', optimizer='adam')
-model.fit(trainX, trainY, epochs = 100, batch_size = 1, verbose = 2)
+model.fit(trainX, trainY, epochs = 10, batch_size = 1, verbose = 2)
 
 #run the model here
 trainPredict = model.predict(trainX)
 testPredict = model.predict(testX)
+model.summary()
+print("Inputs: {}".format(model.input_shape))
+print("Outputs: {}".format(model.output_shape))
+print("Actual input: {}".format(trainX.shape))
+print("Actual output: {}".format(trainPredict.shape))
+print(repr(testY[57]))
+print(repr(testPredict[57]))
 # invert predictions
-trainPredict = scaler.inverse_transform(trainPredict)
-trainY = scaler.inverse_transform([trainY])
-testPredict = scaler.inverse_transform(testPredict)
-testY = scaler.inverse_transform([testY])
+#trainPredict = scaler.inverse_transform(trainPredict)
+#trainY = scaler.inverse_transform([trainY])
+#testPredict = scaler.inverse_transform(testPredict)
+#testY = scaler.inverse_transform([testY])
 # calculate root mean squared error
-trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
+trainScore = math.sqrt(mean_squared_error(trainY, trainPredict))
 print('Train Score: %.2f RMSE' % (trainScore))
-testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
+testScore = math.sqrt(mean_squared_error(testY, testPredict))
 print('Test Score: %.2f RMSE' % (testScore))
-
-# shift train predictions for plotting
-trainPredictPlot = numpy.empty_like(dataset)
-trainPredictPlot[:, :] = numpy.nan
-trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
-# shift test predictions for plotting
-testPredictPlot = numpy.empty_like(dataset)
-testPredictPlot[:, :] = numpy.nan
-testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
-# plot baseline and predictions
-plt.plot(scaler.inverse_transform(dataset))
-plt.plot(trainPredictPlot)
-plt.plot(testPredictPlot)
-plt.show()
 

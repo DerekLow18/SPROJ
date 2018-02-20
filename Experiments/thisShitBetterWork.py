f1fb204c6361376fb972d552390c79ebe0dc76cf
Derek <dlow148@gmail.com>
2018-02-19 22:50:36 -0500

    edits to intro
    Former-commit-id: ad7e4ceb9f7e4dbba418d4323fd3aa46d58b1523

--- Experiments/thisShitBetterWork.py
+++ Experiments/thisShitBetterWork.py
@@ -4,6 +4,7 @@ import pandas
 import math
 import matplotlib.pyplot as plt
 import scipy.spatial
+from sympy import symbols, diff	
 
 # convert an array of values into a dataset matrix
 def create_dataset(dataset, look_back=1):
@@ -52,17 +53,20 @@ print(dataset)
 
 #intilizalize the weight array
 weights = np.random.rand(10,10)
-print(weights)
 
+#error calculation between the predicted step and the actual step, euclidean distance
+def error(prediction, actual):
+	return scipy.spatial.distance.euclidean(prediction, actual)
 
+def pdEuclideanDistance(predicted,actual):#calculate value for partial deriv of euclidean distance w.r.t. predicted
+	return (predicted-actual)/(np.sqrt((predicted-actual)**2))
 
-#formula for the prediction of what the next step will look like. Currently, it's at simple thresholding
+#formula for the prediction of what the next step will look like. Currently, it's at sigmoind function
 def activation(activity):
-	output = 0
-	if activity == 1:
-		output = 1
-	else: output = 0
-	return output
+	return 1 / (1 + math.exp(-activity))
+
+def pdSigmoid(x):
+	return x*(1-x)
 
 #takes a timeStep and attempts to predict the next time step
 def prediction(timeStep):
@@ -71,18 +75,37 @@ def prediction(timeStep):
 	adjustedStep = np.matmul(timeStep, weights)
 	#go through all values of the adjusted step matrix, and multiply them by the activation function
 	for value in range(len(adjustedStep)):
-		adjustedStep[value] = adjustedStep[value]*activation(timeStep[value])
+		adjustedStep[value] = activation(adjustedStep[value])
 		#print(adjustedStep)
 	#return the resulting and final adjusted step
 	return adjustedStep
 
-#error calculation between the predicted step and the actual step, euclidean distance
-def error(prediction, actual):
-	return scipy.spatial.distance.euclidean(prediction, actual)
+'''
+change the weight between one source neuron and the target neuron
+
+'''
+def weightChange(predicted,actual,activity,priorStep):
+	'''x, y= symbols('x y', real=True)
+	f = ((x-y)**2)**(1/2)
+	deriv = diff(f, x)
+	print(deriv)'''
+	i = pdEuclideanDistance(predicted,actual) #partial derivative of euclidean distance with respect to the prediction
+	j = pdSigmoid(activation(activity)) #partial derivative of activation function with respect to the activity
+	totalChange = i*j*priorStep
+	print(totalChange)
+	return totalChange
 
 #main network training function
-def trainNetwork(Max_iters = 10):
+def trainNetwork(Max_iters = 1):
+	predictionMatrix = []
 	for i in range(len(dataset)):
+		predictionMatrix[i] = prediction(dataset[i]);
+		for predicted in range(len(dataset[i])):
+			if predictionMatrix[i][predicted] != dataset[i][predicted]:
+				
+
+
 		print("This is the error: " + str(error(prediction(dataset[i]),dataset[i])))
 
 trainNetwork()
+weightChange(0,1,0,1)

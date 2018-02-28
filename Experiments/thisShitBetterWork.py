6471d1c949ab7e3aba86533080e5cb145f397c5b
Derek <dlow148@gmail.com>
2018-02-27 23:03:54 -0500

    ITS ALMOST WORKINGgit add .
    Former-commit-id: 556178701bd17751b694b03fcc90451c96cb5d7a

--- Experiments/thisShitBetterWork.py
+++ Experiments/thisShitBetterWork.py
@@ -2,9 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas
 import math
-import matplotlib.pyplot as plt
-import scipy.spatial
-from sympy import symbols, diff	
+#import scipy.spatial
+import copy
+#from sympy import symbols, diff
 
 # convert an array of values into a dataset matrix
 '''
@@ -55,14 +55,15 @@ print(dataset.shape)
 print(dataset)
 '''
 #making sure it works on a smaller example
-dataset =[[0.05,0.10],[0.1,0.99]]
-hidden_layer_weights = [[0.15,0.20],[0.25,0.30]]
-output_layer_weights = [[0.40,0.45],[0.50,0.55]]
+dataset =[[0.05,0.10],[0.01,0.99]]
+hidden_layer_weights = [[0.15,0.25],[0.20,0.30]]
+output_layer_weights = [[0.40,0.50],[0.45,0.55]]
 hidden_layer_bias = 0.35
 output_layer_bias = 0.6
 
 #intilizalize the weight array
 weights = np.random.rand(10,10)
+learningRate = 0.5
 
 #error calculation between the predicted step and the actual step, euclidean distance
 def error(prediction, actual):
@@ -77,9 +78,10 @@ def squaredError(prediction,actual):
 #formula for the prediction of what the next step will look like.
 #Currently, it's at sigmoind function
 def activation(activity):
-	return 1 / (1 + math.exp(-activity))
+	return round(1 / (1 + math.exp(-activity)),9)
 
-def pd
+def pdSquaredError(predicted, actual):
+	return round(-(actual - predicted),9)
 
 def pdEuclideanDistance(predicted,actual):
 #calculate value for partial deriv of euclidean distance w.r.t. predicted
@@ -87,29 +89,36 @@ def pdEuclideanDistance(predicted,actual):
 
 #partial derivative of the activation function
 def pdSigmoid(x):
-	return x*(1-x)
+	return round(x*(1-x),9)
 
 #using this for the purposes of testing a standard prediction formula
 def prediction(timeStep):
-	#first calculate the hidden layer values
+	#first calculate the hidden layer values, called prediction array
 	predictionArray = [0,0]
+	predictionActivity = [0,0]
 	for hiddenIndex in range(len(predictionArray)):
 		predicted = 0
 		for inputIndex in range(len(dataset[0])):
-			predicted += dataset[0][inputIndex]*hidden_layer_weights[hiddenIndex][inputIndex]
+			predicted += dataset[0][inputIndex]*hidden_layer_weights[inputIndex][hiddenIndex]
 		predicted = predicted + hidden_layer_bias
-		predictionArray[hiddenIndex] = activation(predicted)
+		predictionActivity[hiddenIndex] = round(predicted,9)
+		predictionArray[hiddenIndex] = round(activation(predicted),9)
+	'''
+	now we calculate the values for the output array
+	'''
 	outputArray = [0,0]
+	outputActivity = [0,0]
 	for outputIndex in range(len(outputArray)):
 		predicted = 0
 		for hiddenIndex in range(len(predictionArray)):
-			predicted += predictionArray[hiddenIndex]*output_layer_weights[outputIndex][hiddenIndex]
+			predicted += predictionArray[hiddenIndex]*output_layer_weights[hiddenIndex][outputIndex]
 		predicted = predicted + output_layer_bias
-		outputArray[outputIndex] = activation(predicted)
+		outputActivity[outputIndex] = round(predicted,9)
+		outputArray[outputIndex] = round(activation(predicted),9)
 
 	print(predictionArray)
 	print(outputArray)
-	return predictionArray, outputArray
+	return [predictionArray, outputArray, predictionActivity, outputActivity]
 
 #takes a timeStep and attempts to predict the next time step
 '''
@@ -118,7 +127,7 @@ def prediction(timeStep):
 	#matrix multiply the weight matrix with the spiking matrix
 	adjustedStep = np.matmul(timeStep, weights)
 	#go through all values of the adjusted step matrix,
-        #and multiply them by the activation function
+		#and multiply them by the activation function
 	for value in range(len(adjustedStep)):
 		adjustedStep[value] = activation(adjustedStep[value])
 		#print(adjustedStep)
@@ -130,34 +139,58 @@ def prediction(timeStep):
 change the weight between one source neuron and the target neuron
 
 '''
-def weightChange(predicted,actual,activity,priorStep):
+def weightChangeOutput(predicted,actual,priorStep):
 	'''x, y= symbols('x y', real=True)
 	f = ((x-y)**2)**(1/2)
 	deriv = diff(f, x)
 	print(deriv)'''
-	i = pdEuclideanDistance(predicted,actual)
-        #partial derivative of euclidean distance with respect to the prediction
-	j = pdSigmoid(activation(activity))
-        #partial derivative of activation function with respect to the activity
-	totalChange = i*j*priorStep
-	print(totalChange)
+	print("priorStep is",priorStep)
+	i = round(pdSquaredError(predicted,actual),9)
+	print("the pd squared error is ", i)
+	#partial derivative of euclidean distance with respect to the prediction
+	print("the activity is", predicted)
+	j = round(pdSigmoid(predicted),9)
+	print("the pd sigmoid is",j)
+		#partial derivative of activation function with respect to the activity
+	totalChange = round(i*j*priorStep,9)
 	return totalChange
+
+def weightChangeHidden(predicted,actual,priorStep):
+	return
 
 #main network training function
 def trainNetwork(Max_iters = 1):
-	predictionMatrix = []
-        #Iterates through all values in the data set
-	for i in range(len(dataset)):
-                #predict the value for the next step and store it
-		predictionMatrix[i] = prediction(dataset[i]);
+	#predictionMatrix = []
+	#Iterates through all values in the data set
+	#for i in range(len(dataset)):
+		#predict the value for the next step and store it
+	predictionMatrix = prediction(dataset[0]);#store the predictions for the array into a matrix
+	'''
 		for predicted in range(len(dataset[i])):
-                        #check to see if prediction and actual are different
+						#check to see if prediction and actual are different
 			if predictionMatrix[i][predicted] != dataset[i][predicted]:
 				return
+				'''
+	'''
+	now that we have the predictions, we need to calculate the weight change for each weight in the
+	weight matrix. Start with the output layer's weights from the hidden layer
+	'''
+	updatedWeights = copy.deepcopy(output_layer_weights)
+	for weightArrayIndex in range(len(output_layer_weights)):
+		for weightValueIndex in range(len(output_layer_weights[weightArrayIndex])):
+			print("Calculating for ", weightArrayIndex,weightValueIndex)
+			weightValue = output_layer_weights[weightArrayIndex][weightValueIndex]
+			weightDelta=weightChangeOutput(predictionMatrix[1][weightValueIndex],dataset[1][weightValueIndex],predictionMatrix[0][weightArrayIndex])
+			print("degree of change is", weightDelta)
+			updatedWeights[weightArrayIndex][weightValueIndex] = weightValue - learningRate*weightDelta
+	print(updatedWeights)
+	'''
+	now we do a similar thing for the input to hidden layer weights.
+	'''
 
 
-		print("This is the error: " + str(error(prediction(dataset[i]),dataset[i])))
+		#print("This is the error: " + str(error(prediction(dataset[i]),dataset[i])))
 
 #trainNetwork()
 #weightChange(0,1,0,1)
-prediction(dataset[0])
+trainNetwork()
